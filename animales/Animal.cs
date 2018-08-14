969ba48249c0a80a8d8aa159f242f436af556422
americaromero <am_mica@hotmail.com>
2018-08-13 22:41:52 -0500

    Métodos y clases POO

---
+++ animales/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Animal
+    {
+        public string especie, clase, familia, genero;
+        public float tamano;
+
+        public Animal(string espe, float tam)
+        {
+            especie = espe;
+            tamano = tam;
+
+        }
+
+        public Animal(string espe, float tam, string cla, string fam, string genre)
+        {
+            especie = espe;
+            tamano = tam;
+            clase = cla;
+            familia = fam;
+            genero = genre;
+     
+        }
+
+        public void comer(string comida)
+        {
+            Console.WriteLine("El animal de nombre" + " "+ especie + " está comiendo " +  comida);
+        }
+
+
+        public void respirar( string nombre, string medio)
+        {
+
+            Console.WriteLine(nombre + " respira en el " + medio );
+        }
+
+
+
+  
+       ////Encapsulamineto CS
+       //modificadore de acceso
+
+
+        //comer
+
+       //respirar
+
+       //reproducen
+
+
+    }
+}
---
+++ animales/Ejercicio2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string medio;
+
+           /* Animal Reptil = new Animal("Reptil",5);
+
+            Reptil.comer("hierba");*/
+
+            Console.WriteLine("Dime el nombre del animal:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Dime dónde respira:");
+            medio = Console.ReadLine();
+
+            Animal Reptil2 = new Animal(nombre, 5);
+
+            Reptil2.comer("carne");
+
+            Animal Hervíboro = new Animal(nombre, 3);
+
+            Hervíboro.respirar(nombre, medio);
+;
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
