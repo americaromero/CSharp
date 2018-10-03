204de837debe572b8743804f79639c6a9ee344db
americaromero <am_mica@hotmail.com>
2018-10-03 17:43:03 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PruebaDeLista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtCuadro = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(184, 62);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtCuadro
+            // 
+            this.txtCuadro.Location = new System.Drawing.Point(184, 150);
+            this.txtCuadro.Multiline = true;
+            this.txtCuadro.Name = "txtCuadro";
+            this.txtCuadro.Size = new System.Drawing.Size(347, 179);
+            this.txtCuadro.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCuadro);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtCuadro;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BibliotecaListas2;
+
+namespace PruebaDeLista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Lista Lista1 = new Lista("");
+            bool b = true;
+            char c = '%';
+            int i = 45;
+            string s = "Hola";
+            Lista1.InsertarAlFrente(b);
+            Lista1.InsertarAlFrente(c);
+            Lista1.InsertarAlFrente(i);
+            Lista1.InsertarAlFrente(s);
+            txtCuadro.Text = Lista1.ImprimirLista(); 
+
+        }
+    }
+}
